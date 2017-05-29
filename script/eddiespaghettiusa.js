8b879c7b3a90e85521b9e15d49406a5eb12fb7b7
Terry Bisiar <t.bisiar@auckland.ac.nz>
2017-05-29 21:32:32 +1200

    Working commit, starting to convert to Bootstrap4 SPA layout, still having layout issues with left navbar so just using flex columns for now.  Need to add sm size collapse functionality

--- script/eddiespaghettiusa.js
+++ script/eddiespaghettiusa.js
@@ -1,29 +1,29 @@
 
 
 var Image1 = new Image();
-Image1.src = "../image/a1.jpg";
+Image1.src = "image/a1.jpg";
 var Image2 = new Image();
-Image2.src = "../image/a1-1.jpg";
+Image2.src = "image/a1-1.jpg";
 var Image3 = new Image();
-Image3.src = "../image/c1.jpg";
+Image3.src = "image/c1.jpg";
 var Image4 = new Image();
-Image4.src = "../image/c1-1.jpg";
+Image4.src = "image/c1-1.jpg";
 var Image5 = new Image();
-Image5.src = "../image/a2.jpg";
+Image5.src = "image/a2.jpg";
 var Image6 = new Image();
-Image6.src = "../image/a2-1.jpg";
+Image6.src = "image/a2-1.jpg";
 var Image7 = new Image();
-Image7.src = "../image/c2.jpg";
+Image7.src = "image/c2.jpg";
 var Image8 = new Image();
-Image8.src = "../image/c2-1.gif";
+Image8.src = "image/c2-1.gif";
 var Image9 = new Image();
-Image9.src = "../image/b1.jpg";
+Image9.src = "image/b1.jpg";
 var Image10 = new Image();
-Image10.src = "../image/b1-1.jpg";
+Image10.src = "image/b1-1.jpg";
 var Image11 = new Image();
-Image11.src = "../image/a1-2.jpg";
+Image11.src = "image/a1-2.jpg";
 var Image12 = new Image();
-Image12.src = "../image/c1-2.jpg";
+Image12.src = "image/c1-2.jpg";
 
 
 function SwapOuta() {
